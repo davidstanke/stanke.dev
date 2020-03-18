75d5d7bc21cf18071a03fe77c641f2469120552b
Dave Stanke <davidstanke@gmail.com>
2020-03-18 18:09:59 +0000

    change GITHUB_TOKEN to env var

--- tools/pr-commenter/pr_comment_post.sh
+++ tools/pr-commenter/pr_comment_post.sh
@@ -3,21 +3,26 @@ set -eEou pipefail
 
 # TODO: rewrite this script as a go(?) binary and provide proper parameter parsing
 
+# expected environment var:
+# $GITHUB_TOKEN
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Env var GITHUB_TOKEN is required for pr_comment_post.sh. Exiting."
+    exit 1
+fi
+
 # parameters:
 # $1: the number of the PR to post to
 # $2: the repo (as org/repo) containing the PR
 # $3: the GitHub access token for an account with PR comment permissions
-# $4: text of the comment to post
 
 PR_NUMBER=$1
 REPO=$2
 COMMENT=$3
-ACCESS_TOKEN=$4
-
 
 PAYLOAD="{\"body\":\"$COMMENT\"}"
 PR_URL="https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments"
 
 echo "posting $PAYLOAD to $PR_URL..."
 
-curl -s -H "Authorization: token $ACCESS_TOKEN" -X POST -d "$PAYLOAD" $PR_URL
+curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST -d "$PAYLOAD" $PR_URL
